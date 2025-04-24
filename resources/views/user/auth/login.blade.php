cbfc4115e98ebc0ea93c963b857f3cec59e1a222
PraveenKumarDreamsTeachnologies <167163753+PraveenKumarDreamsTeachnologies@users.noreply.github.com>
2025-04-24 14:02:58 +0530

    LandingPagesChnages

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -93,11 +93,11 @@
                                     <h6 class="fs-16 mb-1">{{ __('User Login Info') }}:</h6>
                                     <div class="p-3 border d-flex flex-wrap align-items-center justify-content-between">
                                         <div>
-                                            <span class="d-block"><strong>{{ __('Email') }} </strong>: demouser@gmail.com</span>
+                                            <span class="d-block"><strong>{{ __('Email') }} </strong>: demouser@example.com</span>
                                             <span class="d-block"><strong>{{ __('Password') }} </strong>: 12345678</span>
                                         </div>
                                         <div>
-                                            <a class="btn btn-primary copy-login-details" data-email="demouser@gmail.com"
+                                            <a class="btn btn-primary copy-login-details" data-email="demouser@example.com"
                                                 data-password="12345678">
                                                 <i class="far fa-copy"></i>
                                             </a>
