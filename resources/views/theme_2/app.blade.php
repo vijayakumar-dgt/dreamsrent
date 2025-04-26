ff4725e2ddfffcfce057b5a700457db6bd4e2c50
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-04-26 12:48:10 +0530

    changes

--- resources/views/theme_2/app.blade.php
+++ resources/views/theme_2/app.blade.php
@@ -57,7 +57,7 @@
     @else
 	<link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
     @endif
-	<link rel="stylesheet" href="{{ asset('assets/css/custom/custom-style.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/custom/custom-style.css?v=1.2') }}">
 
 </head>
 <body data-theme={{ $theme ?? 1}} data-dir="{{ $isRTL ? 'rtl' : 'ltr' }}">
