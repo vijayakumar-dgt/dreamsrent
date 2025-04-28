fa1a58d4bef94b0685bee3e1bfb621c61789e695
Ranjith <ranjith.madeswaran@dreamstechnologies.com>
2025-04-28 21:17:33 +0530

    Changes

--- public/assets/js/general_setting/system-settings.js
+++ public/assets/js/general_setting/system-settings.js
@@ -6,79 +6,63 @@
     DbBackUpTable();
 
 function DbBackUpTable() {
-    $(document).ready(function() {
-        $.ajax({
-            url: '/admin/settings/system-backup/list',
-            method: 'GET',
-            dataType: 'json',
-            success: function(response) {
-                let tableBody = $("#system-backup-list");
-                tableBody.empty();
-                if(response.data.length === 0){
-                    tableBody.append(`
-                        <tr>
-                            <td colspan="3">
-                                <p class="text-gray-9 text-center">${_l('admin.common.empty_table')}</p>
-                            </td>
-                        </tr>
-                    `);
-                }
-                response.data.forEach(backup => {
-                    let row = `
-                        <tr>
-                            <td>
-                                <h6 class="fw-semibold fs-14">
-                                    <a href="${backup.download_url}" download>${backup.name}</a>
-                                </h6>
-                            </td>
-                            <td>
-                                <p class="text-gray-9">${backup.created_on}</p>
-                            </td>
-                            <td>
-                                <div class="dropdown">
-                                    <button class="btn btn-icon btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="ti ti-dots-vertical"></i>
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-end p-2">
-                                    ${ hasPermission(permissions, 'other_settings', 'edit') ?
-
-                                        `<li>
-                                            <a class="dropdown-item rounded-1" href="javascript:void(0);" onclick="restoreBackup('${backup.name}')">
-                                                <i class="ti ti-restore me-1"></i>${_l('admin.general_settings.restore')}
-                                            </a>
-                                        </li>`:''}
-                                        ${ hasPermission(permissions, 'other_settings', 'delete') ?
-
-                                        `<li>
-                                            <a class="dropdown-item rounded-1" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#delete_backup" onclick="deleteSystemBackup(${backup.id})">
-                                                <i class="ti ti-trash me-1"></i>${_l('admin.general_settings.delete')}
-                                            </a>
-                                        </li>`:''}
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                    `;
-                    tableBody.append(row);
-                });
-            },
-            error: function(error) {
-                console.error("Error fetching backups:", error);
+    $.ajax({
+        url: '/admin/settings/system-backup/list',
+        method: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            let tableBody = $("#system-backup-list");
+            tableBody.empty();
+            if(response.data.length === 0){
+                tableBody.append(`
+                    <tr>
+                        <td colspan="4">
+                            <p class="text-gray-9 text-center">${_l('admin.common.empty_table')}</p>
+                        </td>
+                    </tr>
+                `);
             }
-        });
+            response.data.forEach(backup => {
+                let row = `
+                    <tr>
+                        <td>
+                            <h6 class="fw-semibold fs-14">
+                                <a href="${backup.download_url}" download>${backup.name}</a>
+                            </h6>
+                        </td>
+                        <td>
+                            <p class="text-gray-9">${backup.created_on}</p>
+                        </td>
+                        <td>
+                            <div class="dropdown">
+                                <button class="btn btn-icon btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="ti ti-dots-vertical"></i>
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end p-2">
+                                    <li>
+                                        <a class="dropdown-item rounded-1" href="${backup.download_url}" download>
+                                            <i class="ti ti-download me-1"></i>${_l('admin.common.download')}
+                                        </a>
+                                    </li>
+                                    ${ hasPermission(permissions, 'other_settings', 'delete') ?
+
+                                    `<li>
+                                        <a class="dropdown-item rounded-1" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#delete_backup" onclick="deleteSystemBackup(${backup.id})">
+                                            <i class="ti ti-trash me-1"></i>${_l('admin.general_settings.delete')}
+                                        </a>
+                                    </li>`:''}
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                `;
+                tableBody.append(row);
+            });
+        },
+        error: function(error) {
+            console.error("Error fetching backups:", error);
+        }
     });
-
-}
-
-})();
-
-
-function restoreBackup(filename) {
-    alert("Restore function for " + filename + " will be implemented here.");
-}
-
-function deleteSystemBackup(id){
-    $("#delete_id").val(id);
 }
 
 $("#deleteSystemDbBackup").on('submit', function(e){
@@ -110,4 +94,15 @@ $("#deleteSystemDbBackup").on('submit', function(e){
 
     });
 });
+
+})();
+
+
+function restoreBackup(filename) {
+    alert("Restore function for " + filename + " will be implemented here.");
+}
+
+function deleteSystemBackup(id){
+    $("#delete_id").val(id);
+}
 
