e0aca75b8ec2f49bb402ed94770d739fc11e5f4f
preethis <preethi.selvaraj@dreamstechnologies.com>
2025-05-02 11:21:23 +0530

    larastan

--- Modules/GeneralSetting/app/Models/UserDevice.php
+++ Modules/GeneralSetting/app/Models/UserDevice.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 // use Modules\GeneralSetting\Database\Factories\UserDeviceFactory;
 
+/** @property int $user_id
+ *  @property string|null $device_type
+ *  @property string|null $browser
+ *  @property string|null $os
+ *  @property string|null $ip_address
+ *  @property string|null $location
+ */
 class UserDevice extends Model
 {
     use HasFactory;
