c04b93add9d5b7919217cd7d1d27f86ac63de8eb
Thangamani <thangamani.c@dreamstechnologies.com>
2025-05-02 17:41:04 +0530

    larastan changes report module

--- Modules/Report/app/Providers/ReportServiceProvider.php
+++ Modules/Report/app/Providers/ReportServiceProvider.php
@@ -80,23 +80,35 @@ class ReportServiceProvider extends ServiceProvider
     {
         $relativeConfigPath = config('modules.paths.generator.config.path');
         $configPath = module_path($this->name, $relativeConfigPath);
-
+    
         if (is_dir($configPath)) {
             $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($configPath));
-
+    
             foreach ($iterator as $file) {
                 if ($file->isFile() && $file->getExtension() === 'php') {
                     $relativePath = str_replace($configPath . DIRECTORY_SEPARATOR, '', $file->getPathname());
-                    $configKey = $this->nameLower . '.' . str_replace([DIRECTORY_SEPARATOR, '.php'], ['.', ''], $relativePath);
+    
+                    // Ensure the result is a string to avoid Larastan error
+                    $sanitized = str_replace(
+                        [DIRECTORY_SEPARATOR, '.php'],
+                        ['.', ''],
+                        $relativePath
+                    );
+    
+                    if (is_array($sanitized)) {
+                        $sanitized = implode('', $sanitized); // fallback safety, though it shouldn't be an array
+                    }
+    
+                    $configKey = $this->nameLower . '.' . (string) $sanitized;
                     $key = ($relativePath === 'config.php') ? $this->nameLower : $configKey;
-
+    
                     $this->publishes([$file->getPathname() => config_path($relativePath)], 'config');
                     $this->mergeConfigFrom($file->getPathname(), $key);
                 }
             }
         }
     }
-
+    
     /**
      * Register views.
      */
