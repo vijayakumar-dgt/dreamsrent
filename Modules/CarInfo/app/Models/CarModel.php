3154aaa58e2ace4be2737d7b4faa4ca042a1d348
DevaDreams <devarajan.raja@dreamstechnologies.com>
2025-05-05 13:03:47 +0530

    changes

--- Modules/CarInfo/app/Models/CarModel.php
+++ Modules/CarInfo/app/Models/CarModel.php
@@ -6,27 +6,47 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-// use Modules\CarInfo\Database\Factories\CarModelFactory;
-
+/**
+ * Class CarModel
+ * 
+ * @property int $id
+ * @property int|null $language_id
+ * @property string $model_name
+ * @property int $brand_id
+ * @property int $total_cars
+ * @property int $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read Brand $brand
+ */
 class CarModel extends Model
 {
     use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
+     *
+     * @var array
      */
     protected $fillable = [
         'language_id',
         'model_name',
         'brand_id',
         'total_cars',
-        'status'
+        'status',
+        'created_at',
+        'updated_at',
+        'deleted_at',
     ];
+
     /**
-     * @return BelongsTo<Brand, CarModel> */
-    public function brands(): BelongsTo
+     * Get the brand associated with the car model.
+     *
+     * @return BelongsTo<Brand, CarModel>
+     */
+    public function brand(): BelongsTo
     {
-        /** @var BelongsTo<Brand, CarModel> */
         return $this->belongsTo(Brand::class, 'brand_id');
     }
 }
