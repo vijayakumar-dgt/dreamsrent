7bf8feb3ab8533e110c81357198f5b71e2de8ca4
PraveenKumarDreamsTeachnologies <167163753+PraveenKumarDreamsTeachnologies@users.noreply.github.com>
2025-05-05 18:00:39 +0530

    changes

--- app/Helpers/CommonHelpers.php
+++ app/Helpers/CommonHelpers.php
@@ -111,12 +111,12 @@ if (!function_exists('uploadedAsset')) {
 
         // Default response structure
         $defaultImages = [
-            'profile' => $baseUrl . '/assets/img/default-profile.png',
-            'default2' => $baseUrl . '/assets/img/default-placeholder-image.png',
-            'default' => $baseUrl . '/assets/img/default-image-02.jpg',
-            'default_logo' => $baseUrl . '/assets/img/logo.svg',
+            'profile' => $baseUrl . '/backend/assets/img/default-profile.png',
+            'default2' => $baseUrl . '/backend/assets/img/default-placeholder-image.png',
+            'default' => $baseUrl . '/backend/assets/img/default-image-02.jpg',
+            'default_logo' => $baseUrl . '/backend/assets/img/logo.svg',
             'default_small_logo' => $baseUrl . '/frontend/assets/img/logo-small.png',
-            'default_favicon' => $baseUrl . '/assets/img/favicon.png',
+            'default_favicon' => $baseUrl . '/backend/assets/img/favicon.png',
         ];
 
         // If file does not exist, return default image
@@ -152,12 +152,12 @@ if (!function_exists('uploadedAssetDetails')) {
 
         // Default response structure
         $defaultImages = [
-            'profile' => $baseUrl . '/assets/img/default-profile.png',
-            'default2' => $baseUrl . '/assets/img/default-placeholder-image.png',
-            'default' => $baseUrl . '/assets/img/default-image-02.jpg',
-            'default_logo' => $baseUrl . '/assets/img/logo.svg',
+            'profile' => $baseUrl . '/backend/assets/img/default-profile.png',
+            'default2' => $baseUrl . '/backend/assets/img/default-placeholder-image.png',
+            'default' => $baseUrl . '/backend/assets/img/default-image-02.jpg',
+            'default_logo' => $baseUrl . '/backend/assets/img/logo.svg',
             'default_small_logo' => $baseUrl . '/frontend/assets/img/logo-small.png',
-            'default_favicon' => $baseUrl . '/assets/img/favicon.png',
+            'default_favicon' => $baseUrl . '/backend/assets/img/favicon.png',
         ];
 
         // If file does not exist, return default image
