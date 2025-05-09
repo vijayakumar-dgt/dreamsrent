d867280a8919520e26cf119e0f2ea4982e37bdca
DevaDreams <devarajan.raja@dreamstechnologies.com>
2025-05-09 16:00:08 +0530

    changes

--- public/frontend/assets/js/custom/user/bookings.js
+++ public/frontend/assets/js/custom/user/bookings.js
@@ -253,13 +253,26 @@ const renderButtons = data => {
     let html = '';
     switch (data.status) {
         case 4:
-            html += `<a href="javascript:void(0);" id="cancel_booking" data-id="${data.id}" class="btn btn-sm btn-secondary">${_l('web.common.cancel')} ${_l('web.user.booking')}</a>`;
+            html += `<a href="javascript:void(0);" id="cancel_booking" data-id="${data.id}" class="btn  me-2 btn-sm btn-secondary">
+                        ${_l('web.common.cancel')} ${_l('web.user.booking')}
+                    </a>`;
             if (showStartRideButton) {
-                html += `<a href="javascript:void(0);" id="start_ride" data-id="${data.id}" class="btn btn-sm btn-primary">${_l('web.user.start_ride')}</a>`;
+                html += `<a href="javascript:void(0);" id="start_ride" data-id="${data.id}" class="btn  btn-sm btn-primary">
+                            ${_l('web.user.start_ride')}
+                        </a>`;
             }
             break;
+        case 1:
+            html += `<a href="javascript:void(0);" id="complete_booking" data-id="${data.id}" class="btn btn-sm btn-primary">
+                        ${_l('web.user.complete_ride')}
+                     </a>`;
+            break;
+    
+        default:
+            html = '';
+            break;
     }
-    $(".modal_footer").html(html);
+    $(".modal_footer").html(html);    
 };
 
 // Fetch & Show Booking Details
