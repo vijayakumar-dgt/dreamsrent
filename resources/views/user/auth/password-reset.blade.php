ab09a2602ad7057423496fc09d06230dc75c2c91
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-05-09 15:10:55 +0530

    update: login,register,pages header alignment

--- resources/views/user/auth/password-reset.blade.php
+++ resources/views/user/auth/password-reset.blade.php
@@ -4,16 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Dreams Rent | ForgotPassword</title>
+    <title>{{ isset($seo_title) ? $seo_title : config('app.name') }} | ForgotPassword</title>
+
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon.png') }}">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
+
     <!-- Fontawesome CSS -->
     <link rel="stylesheet" href="{{ asset('frontend/assets/plugins/fontawesome/css/fontawesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/assets/plugins/fontawesome/css/all.min.css') }}">
+
     <!-- Fearther CSS -->
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/feather.css') }}">
+
     <!-- Main CSS -->
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
 </head>
@@ -67,20 +72,24 @@
        
     </div>
     <!-- /Main Wrapper -->
+
     <!-- jQuery -->
     <script src="{{ asset('frontend/assets/js/jquery-3.7.1.min.js') }}"></script>
+
     <!-- jQuery validation -->
     <script src="{{ asset('backend/assets/js/jquery/jquery-validation.min.js') }}"></script>
     <script src="{{ asset('backend/assets/js/jquery/jquery-validation-additional-methods.min.js') }}"></script>
+
     <!-- Toastr JS -->
     <script src="{{ asset('backend/assets/plugins/toastr/toastr.min.js') }}"></script>
+
     <!-- Bootstrap Core JS -->
     <script src="{{ asset('frontend/assets/js/bootstrap.bundle.min.js') }}"></script>
+     <!-- Custom JS -->
     <script src="{{ asset('frontend/assets/js/user/login.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/custom/lang_script.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/custom/custom-script.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/user/reset-password.js') }}"></script>
-    <!-- Custom JS -->
     <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
 </body>
 </html>
