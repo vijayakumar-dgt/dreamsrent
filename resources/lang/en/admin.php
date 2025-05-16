07a7cd84ad1671b4a3b4ed515c0f47f772e2391c
PraveenKumarDreamsTeachnologies <167163753+PraveenKumarDreamsTeachnologies@users.noreply.github.com>
2025-05-16 16:06:51 +0530

    language Changes

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -3,6 +3,8 @@
 return array(
   'general_settings' =>
   array(
+    'privacy_policy' => 'Privacy Policy', 
+    'terms_of_use' => 'Terms of Use',   
     'settings_configuration' => 'Settings & Configuration',
     'account_settings' => 'Account Settings',
     'basic_information' => 'Basic Information',
@@ -1920,7 +1922,10 @@ return array(
     'view' => 'View',
     'export' => 'Export',
     'search' => 'Search',
+    'last_days' => 'Last :no Days',
     'latest' => 'Latest',
+    'tarrifs' => 'Tariffs',
+    'tariff' => 'Tariff',
     'ascending' => 'Ascending',
     'descending' => 'Descending',
     'last_month' => 'Last Month',
