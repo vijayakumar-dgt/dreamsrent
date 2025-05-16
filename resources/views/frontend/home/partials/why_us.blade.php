17e2dcb508bd0df44d821812b8c362819cdd4314
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-05-16 13:14:38 +0530

    why us dynamic

--- resources/views/frontend/home/partials/why_us.blade.php
+++ resources/views/frontend/home/partials/why_us.blade.php
@@ -1,4 +1,4 @@
-    <!-- Why Choose Us -->
+<!-- Why Choose Us -->
     <section class="section why-choose popular-explore">
         <div class="choose-left">
             <img src="{{ asset('frontend/assets/img/bg/choose-left.png') }}" class="img-fluid" alt="{{ __('web.home.why_choose_us') }}">
@@ -16,45 +16,23 @@
             <!-- /Heading title -->
             <div class="why-choose-group">
                 <div class="row">
+                    @if(!empty($section['section_content']['items']) && count($section['section_content']['items']) > 0)
+                        @foreach($section['section_content']['items'] as $item)
                     <div class="col-lg-4 col-md-6 col-12 d-flex" data-aos="fade-down">
                         <div class="card flex-fill">
                             <div class="card-body">
                                 <div class="choose-img choose-black">
-                                    <img src="{{ asset('frontend/assets/img/icons/bx-selection.svg') }}" alt="Icon">
+                                    <img src="{{ $item['why_icon'] }}" alt="Icon">
                                 </div>
                                 <div class="choose-content">
-                                    <h4>{{ __('web.home.easy_and_fast_booking') }}</h4>
-                                    <p>{{ __('web.home.fast_booking_content') }}</p>
+                                    <h4>{{ $item['why_label'] }}</h4>
+                                    <p>{{ $item['why_dis'] }}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 col-12 d-flex" data-aos="fade-down">
-                        <div class="card flex-fill">
-                            <div class="card-body">
-                                <div class="choose-img choose-secondary">
-                                    <img src="{{ asset('frontend/assets/img/icons/bx-crown.svg') }}" alt="Icon">
-                                </div>
-                                <div class="choose-content">
-                                    <h4>{{ __('web.home.many_pickup_locations') }}</h4>
-                                    <p>{{ __('web.home.many_pickup_locations_content') }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-12 d-flex" data-aos="fade-down">
-                        <div class="card flex-fill">
-                            <div class="card-body">
-                                <div class="choose-img choose-primary">
-                                    <img src="{{ asset('frontend/assets/img/icons/bx-user-check.svg') }}" alt="Icon">
-                                </div>
-                                <div class="choose-content">
-                                    <h4>{{ __('web.home.customer_satisfaction') }}</h4>
-                                    <p>{{ __('web.home.customer_satisfaction_content')  }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
+                    @endif
                 </div>
             </div>
         </div>
