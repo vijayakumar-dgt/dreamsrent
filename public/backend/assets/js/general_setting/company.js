27a11cd645c511a210f3535685b72a6e30235046
PraveenKumarDreamsTeachnologies <167163753+PraveenKumarDreamsTeachnologies@users.noreply.github.com>
2025-05-21 11:40:15 +0530

    Changes Settings

--- public/backend/assets/js/general_setting/company.js
+++ public/backend/assets/js/general_setting/company.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 (async () => {
     await loadTranslationFile("admin", "general_settings,common");
-    ("use strict");
+    "use strict";
     $(document).ready(function () {
         company_list();
 
