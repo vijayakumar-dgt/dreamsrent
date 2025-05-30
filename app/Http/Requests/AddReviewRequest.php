be240d13545a1ae2cdaf3e32fec53868edfc6b65
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-05-30 17:31:59 +0530

    update: Repository implemented on Review

---
+++ app/Http/Requests/AddReviewRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Library\CustomFailedValidation;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddReviewRequest extends CustomFailedValidation
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'comments' => 'required|min:3',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'comments.required' => __('web.home.comments_required'),
+            'comments.min' => __('web.home.comments_minlength'),
+        ];
+    }
+}
