7a583f7d4f07d1a5b6a056f673c5567855fe214b
madhunath.e <madhunath.eswaran@dreamstechnologies.com>
2025-06-04 08:45:44 +0000

    Edit database.php

--- config/database.php
+++ config/database.php
@@ -162,7 +162,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
+            'username' => env('REDIS_USERAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
