212ad621ca5921c7ad86ace6042e876f2603def4
DevaDreams <devarajan.raja@dreamstechnologies.com>
2025-06-05 17:19:31 +0530

    changes

--- Modules/Page/app/Http/Controllers/PageController.php
+++ Modules/Page/app/Http/Controllers/PageController.php
@@ -868,10 +868,11 @@ class PageController extends Controller
                             ->get();
 
                         // Fetch Facts data
-                        $userCount = User::count(); // Get total users
-                        $vehicleCount = VehicleInfo::count(); // Get total vehicles
-                        $locationCount = Location::count(); // Get total locations
-                        $totalKm = 1976; // Keeping total_km static
+                        $getCategoryId = getCategoryId();
+                        $userCount = User::where("user_type", 3)->count();
+                        $vehicleCount = VehicleInfo::where('category_id', $getCategoryId)->count();
+                        $locationCount = Location::where('language_id', $lang_id)->count();
+                        $totalKm = 1976;
 
                         $facts = [
                             ["key" => "happy_customers", "value" => $userCount],
@@ -2357,7 +2358,7 @@ class PageController extends Controller
                 ->get();
 
             $content_sections = collect((array) $data['content_sections']);
-            // dd($content_sections);
+            dd($content_sections);
             if (request()->has('is_mobile') && request()->get('is_mobile') === "yes") {
                 return response()->json(['code' => "200", 'message' => __('Page details retrieved successfully.'), 'data' => $data], 200);
             } else {
