f39f76f53f7f4d6665f058fe694b5835053c6e96
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-06-05 17:34:03 +0530

    isse fic

--- resources/views/frontend/home/partials_3/feature_vehicles.blade.php
+++ resources/views/frontend/home/partials_3/feature_vehicles.blade.php
@@ -8,7 +8,6 @@
 
     $titleMain = implode(' ', array_slice($titleWords, 0, -$lastPartCount));
     $titleLastPart = implode(' ', array_slice($titleWords, -$lastPartCount));
-    $vehicles = $sectionContent['vehicles'] ?? [];
 @endphp
 <!-- Popular Section -->
 <section class="section popular-section">
@@ -22,8 +21,8 @@
                 </div>
                 <!-- /Heading title -->
                 <div class="row">
-                    @if(!empty($vehicles) && count($vehicles) > 0)
-                    @foreach ($vehicles as $vehicle)
+                    @if(!empty($sectionContent) && count($sectionContent) > 0)
+                    @foreach ($sectionContent as $vehicle)
                     <div class="col-lg-6">
                         <div class="listing-item bike-list">											
                             <div class="listing-img">
