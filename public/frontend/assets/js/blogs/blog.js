a01cdbd30f6266e041b7e07f6ca70c05be823b3f
preethis <preethi.selvaraj@dreamstechnologies.com>
2025-06-06 12:21:00 +0530

    changes

--- public/frontend/assets/js/blogs/blog.js
+++ public/frontend/assets/js/blogs/blog.js
@@ -16,7 +16,8 @@
                     headers: { "X-Requested-With": "XMLHttpRequest" },
                 });
                 const data = await response.json();
-                $blogContainer.text(data.html);
+                let cleanDescription = DOMPurify.sanitize(data.html);
+                $blogContainer.empty().append(cleanDescription);
                 bindPaginationLinks();
 
                 window.scrollTo({
