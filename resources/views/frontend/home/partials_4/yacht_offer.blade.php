fdcb75fb4735304690e451b9e4fe730222f2d16c
Ranjithkumar <ranjithkumar.raman@dreamstechnologies.com>
2025-06-10 19:06:19 +0530

    changes

--- resources/views/frontend/home/partials_4/yacht_offer.blade.php
+++ resources/views/frontend/home/partials_4/yacht_offer.blade.php
@@ -12,7 +12,7 @@
             <div class="yacht-owner-title">
                 <h3>{{ $section['section_title'] ?? "" }}</h3>
                 <p>{{ $section['section_label'] ?? "" }}</p>
-                <a href="listing-details.html" class="btn btn-primary">View Yachts</a>
+                <a href="{{ route('list') }}" class="btn btn-primary">View Yachts</a>
             </div>
         </div>
     </div>
