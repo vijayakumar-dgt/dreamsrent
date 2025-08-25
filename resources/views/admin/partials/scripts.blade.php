fe4a6587f195882d19e15b44de18b8833c128c78
Ganapathy Vadivel <ganapathy.vadivel@dreamstechnologies.com>
2025-08-25 15:11:45 +0530

    Sonarqube: Duplicatedd plugin files removed

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -18,7 +18,7 @@
 <script src="{{ asset('backend/assets/js/jquery.slimscroll.min.js') }}"></script>
 
 <!-- Sticky Sidebar JS -->
-<script src="{{ asset('backend/assets/plugins/theia-sticky-sidebar/ResizeSensor.js') }}"></script>
+<script src="{{ asset('frontend/assets/plugins/theia-sticky-sidebar/ResizeSensor.js') }}"></script>
 <script src="{{ asset('backend/assets/plugins/theia-sticky-sidebar/theia-sticky-sidebar.js') }}"></script>
 
 @if (!Route::is(['admin.permissions', 'admin.customer-recent-rents', 'admin.customer-details', 'admin.newsletters']))
